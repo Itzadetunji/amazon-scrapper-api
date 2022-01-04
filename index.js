8eedf1eaa1769f5a648a511488aea79099436ce6
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2022-01-04 19:42:49 +0100

    Created /products/:id endpoint

--- index.js
+++ index.js
@@ -5,12 +5,23 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const apiKey = "8c12c7a264ed93210d6c52e027266dc8";
-const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}`
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome to Amazon Scraper API');
+})
+
+//GET Product Details
+app.get('/products/:productId', async (req, res) => {
+  const {productId} = req.params;
+  try{
+    const response = await request();
+  }
+  catch (error){
+
+  }
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
