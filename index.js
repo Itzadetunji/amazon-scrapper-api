84d473d5c2d4f024129f20b7211065b7145ab08c
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2022-01-04 17:39:01 +0100

    Added Apikey

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const request = require('request-promise')
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const apiKey = "8c12c7a264ed93210d6c52e027266dc8";
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}`
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
