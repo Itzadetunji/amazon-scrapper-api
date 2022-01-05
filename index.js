06debfa266d81e1cedb1d064e7c3a1a8ed56217a
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2022-01-05 04:06:48 +0100

    Created search endpoint

--- index.js
+++ index.js
@@ -18,7 +18,43 @@ app.get('/products/:productId', async (req, res) => {
   const {productId} = req.params;
   try{
     const response = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productId}`);
-    res.json(response);
+    res.json(JSON.parse(response));
+  }
+  catch (error){
+    res.json(error);
+  }
+})
+
+//GET Product Reviews
+app.get('/products/:productId/reviews', async (req, res) => {
+  const {productId} = req.params;
+  try{
+    const response = await request(`${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`);
+    res.json(JSON.parse(response));
+  }
+  catch (error){
+    res.json(error);
+  }
+})
+
+//GET Product Offers
+app.get('/products/:productId/offers', async (req, res) => {
+  const {productId} = req.params;
+  try{
+    const response = await request(`${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+    res.json(JSON.parse(response));
+  }
+  catch (error){
+    res.json(error);
+  }
+})
+
+//GET Search Results
+app.get('/search/:searchQuery', async (req, res) => {
+  const {searchQuery} = req.params;
+  try{
+    const response = await request(`${baseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`);
+    res.json(JSON.parse(response));
   }
   catch (error){
     res.json(error);
